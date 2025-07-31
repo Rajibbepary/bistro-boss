1bae37985d50036d4ed6e242cbe12affb38d2a34
Rajibbepary <rajib635356@gmail.com>
2025-07-31 22:07:02 +0600

    our menu page done

--- client/src/pages/Menu/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu/Menu.jsx
@@ -3,13 +3,36 @@ import Cover from '../../Shared/Cover/Cover';
 import Bannerimg from '../../../assets/menu/banner3.jpg'
 import dessertsimg from '../../../assets/menu/dessert-bg.jpeg'
 import SectionTitel from '../../../components/SectionTitel/SectionTitel';
-
+import useMenu from '../../../hooks/useMenu';
+import MenuCategory from '../MenuCategory/MenuCategory';
+import pizzaImage from '../../../assets/menu/pizza-bg.jpg'
+import saladImage from '../../../assets/menu/salad-bg.jpg'
+import suopImage from '../../../assets/menu/soup-bg.jpg'
 const Menu = ( ) => {
+const [menu] = useMenu()
+  
+    const salad = menu.filter(item => item.category === 'salad')
+    const drinks = menu.filter(item => item.category === 'drinks')
+    const dessert = menu.filter(item => item.category === 'dessert')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const soup = menu.filter(item => item.category === 'soup')
+    const  offered = menu.filter(item => item.category === 'offered')
+   
     return (
         <div>
            <Cover image={Bannerimg}  titel={'Our Menu'} subtitel={'WORLD YOU LIKE TO TRY DISH?'}/>
            <SectionTitel subHeading={"Don't miss"} heading={"TODAY' OFFER"}/>
+           <MenuCategory items={offered} />
            <Cover image={dessertsimg}  titel={'DESSERTS'} subtitel={'Healthy Gelato Treats — Tiny treats, big satisfaction – indulge in our low-calories, gluten-free mini sticks!'}/>
+            <MenuCategory items={ dessert} />
+              <Cover image={pizzaImage}  titel={'Pizza'} subtitel={'Roasted duck breast (served pink) with gratin potato and a griottine cherry sauce'}/>
+              <MenuCategory items={ pizza} />
+               <Cover image={saladImage}  titel={'Salad'} subtitel={'Sautéed breaded veal escalope with watercress, lemon and veal jus.'}/>
+               <MenuCategory items={ salad} />
+               <Cover image={suopImage}  titel={'Saups'} subtitel={'Pan roasted pork belly with gratin potato, braised Savoy cabbage, apples, thyme and calvados jus'}/>
+                <MenuCategory items={ soup} />
+                 <Cover image={Bannerimg}  titel={'Drinks'} subtitel={'Pan roasted pork belly with gratin potato, braised Savoy cabbage, apples, thyme and calvados jus'}/>
+                  <MenuCategory items={ drinks} />
         </div>
     );
 };
--- client/src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ client/src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,9 +1,19 @@
 
+import MenuItem from "../../Shared/MenuItem/MenuItem";
 
-const MenuCategory = () => {
+
+const MenuCategory = ({items }) => {
     return (
-        <div>
-            
+        <div >
+          
+          <div className="grid w-11/12 my-12 mx-auto md:grid-cols-2 gap-10">
+                {
+                    items.slice(0, 6).map(item=> <MenuItem key={item._id} item={item}/>)
+                }
+            </div>
+            <div className="text-center my-10">
+                <button className="uppercase border-0 border-b-4 rounded-md text-black mt-8 ">order our favourite food</button>
+            </div>
         </div>
     );
 };
