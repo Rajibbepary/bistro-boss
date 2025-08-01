726a2547cd94cc67ae2844e1c3745083cf8481f8
Rajibbepary <rajib635356@gmail.com>
2025-08-01 11:01:40 +0600

    contact full page done

--- client/src/pages/Contact/Contact.jsx
+++ client/src/pages/Contact/Contact.jsx
@@ -4,14 +4,15 @@ import SectionTitel from "../../components/SectionTitel/SectionTitel";
 import { FaPhoneVolume } from "react-icons/fa6";
 import { FaLocationDot } from "react-icons/fa6";
 import { MdAccessTimeFilled } from "react-icons/md";
+import { IoIosSend } from "react-icons/io";
 const Contact = () => {
     return (
-        <div className="py-6">
+        <div className="py-10">
             <Cover image={contactimg} titel={'CONTACT US'} subtitel={'Have a question or need help with your order? Our team is here for you! Reach out anytime—we’d love to hear from you.'}/>
 
             <SectionTitel subHeading={'Visit Us'} heading={'our location'}/>
 
-            <div className="w-10/12 mt-7 mx-auto grid md:grid-cols-3 gap-4">
+            <div className="w-10/12 my-10 mx-auto grid md:grid-cols-3 gap-4">
             {/* card-1 */}
                 <div className=" w-ful bg-white box-border border text-center border-[#e8e8e8]">
                         <div className="w-full flex justify-center items-center p-4 bg-[rgb(209,160,84)]">
@@ -54,6 +55,45 @@ const Contact = () => {
 
                 </div>
             </div>
+
+            <SectionTitel subHeading={'Send Us a Message'} heading={'CONTACT FROM'}/>
+
+            {/* contact from */}
+            <div className="my-10 bg-[#f3f3f3] py-12 w-10/12 mx-auto">
+
+                 <form className="flex flex-col items-center text-sm">
+           
+            
+            <div className="flex flex-col md:flex-row items-center gap-8 w-[350px] md:w-[700px]">
+                <div className="w-full">
+                    <label className="text-black/70" htmlFor="name">Your Name</label>
+                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300" type="text" required />
+                </div>
+                <div className="w-full">
+                    <label className="text-black/70" htmlFor="name">Your Email</label>
+                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300" type="email" required />
+                </div>
+            </div>
+
+            {/* phone */}
+                <div className="flex flex-col mt-3 md:flex-row items-center gap-8 w-[350px] md:w-[700px]">
+                <div className="w-full">
+                    <label className="text-black/70" htmlFor="name">Your Phone</label>
+                    <input className="h-12 p-2 mt-2 w-full border border-gray-500/30 rounded outline-none focus:border-indigo-300" type="text" required />
+                </div>
+            </div>
+
+            
+        
+            <div className="mt-6 w-[350px] md:w-[700px]">
+                <label className="text-black/70" htmlFor="name">Message</label>
+                <textarea className="w-full mt-2 p-2 h-40 border border-gray-500/30 rounded resize-none outline-none focus:border-indigo-300" required></textarea>
+            </div>
+        
+            <button type="submit" className="mt-5 flex justify-center items-center bg-gradient-to-r from-[#835d23] to-[#b58130] text-white h-12 w-56 px-4 rounded active:scale-95 transition">Send Message <IoIosSend className="ml-2 text-xl" /></button>
+        </form>
+            </div>
+
         </div>
     );
 };
