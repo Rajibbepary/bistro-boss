4b3e86c7f9181315db80e44d2d7c8b594dd4e5e0
Rajibbepary <rajib635356@gmail.com>
2025-08-05 09:19:15 +0600

    login from function add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './Routes/Routes';
-
+import { ToastContainer} from 'react-toastify';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
        <div className='max-w-screen-xl mx-auto'>
+        <ToastContainer />
        <RouterProvider router={router} />
      </div>
   
