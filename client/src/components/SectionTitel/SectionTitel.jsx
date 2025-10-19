f50eacb80c256fa2859895870b86c518ccf9b893
Rajibbepary <rajib635356@gmail.com>
2025-10-19 21:33:31 +0600

    few style change

--- client/src/components/SectionTitel/SectionTitel.jsx
+++ client/src/components/SectionTitel/SectionTitel.jsx
@@ -3,8 +3,8 @@
 const SectionTitel = ({heading, subHeading}) => {
     return (
         <div className=" md:w-4/12 mx-auto text-center py-8">
-            <p className="text-yellow-600 mb-2 font-cinzel">--- {subHeading} ---</p>
-            <h3 className="text-2xl font-semibold text-gray-500 uppercase font-cinzel border-y border-slate-300 py-4">{heading}</h3>
+            <p className="text-yellow-500 mb-2 font-cinzel">--- {subHeading} ---</p>
+            <h3 className="text-2xl font-semibold text-gray-700 uppercase font-cinzel border-y border-slate-300 py-4">{heading}</h3>
         </div>
     );
 };
